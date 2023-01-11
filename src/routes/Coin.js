e247b0d5680b421b1d210536770e27004cc03ea3
fbiakbr <febiakbar19@gmail.com>
2023-01-11 14:08:21 +0700

    fix dynamic routing

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const Coin = () => {
-  return (
-    <div>
-        <h1></h1>
-    </div>
-  )
+
+    const params = useParams()
+    const [coin, setCoin] = useState({})
+
+    const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setCoin(response.data)
+            console.log(response.data);
+        }).catch((error) => {
+            console.log(error);
+        })
+    })
+
+    return (
+        <div>
+            <h1>{coin.id}</h1>
+        </div>
+    )
 }
 
 export default Coin
